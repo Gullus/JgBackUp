09692a6c89a615eb01cbf45c5483cff82c32ef16
Jörg Gullus <joerg@gullus.de>
2020-11-07 16:09:32 +0100

    Projektdateien hinzufügen.

---
+++ JgBackUp/Helper.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+using System;
+
+namespace JgBackUp
+{
+    internal static class Helper
+    {
+        public static void Backup(string nameDatenbank, string conString, string dateiAusgabe)
+        {
+            var connection = new SqlConnection(conString);
+            var serverConnection = new ServerConnection(connection);
+            var server = new Server(serverConnection);
+            var database = server.Databases[nameDatenbank];
+            Backup backup = new Backup
+            {
+                Action = BackupActionType.Database,
+                BackupSetDescription = $"{nameDatenbank} - full backup",
+                BackupSetName = $"{nameDatenbank} backup",
+                Database = nameDatenbank,
+                PercentCompleteNotification = 10,
+                Incremental = false,
+                LogTruncation = BackupTruncateLogType.Truncate,
+            };
+
+            var deviceItem = new BackupDeviceItem(dateiAusgabe, DeviceType.File);
+            backup.Devices.Add(deviceItem);
+
+            backup.Information += (sender, e) => Ausgabe(ConsoleColor.White, $"DB Info: {e.ToString().Replace("Microsoft.Data.SqlClient.SqlError: ", "")}");
+            backup.NextMedia += (sender, e) => Ausgabe(ConsoleColor.Gray, $"DB Media: {e.Error}");
+            backup.PercentComplete += (sender, e) => Ausgabe(ConsoleColor.White, $"DB Message: {e.Message}");
+            backup.Complete += (sender, e) => Ausgabe(ConsoleColor.Yellow, "DB BackUp abgeschlossen.");
+
+            backup.SqlBackup(server);
+
+            return;
+        }
+
+        public static void Ausgabe(ConsoleColor farbe, string text)
+        {
+            Console.ForegroundColor = farbe;
+            Console.WriteLine(DateTime.Now.ToString("HH:mm:ss") + " " + text);
+        }
+    }
+}
---
+++ JgBackUp/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace JgBackUp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var pause = false;
+
+            if (args.Length > 0)
+            {
+                if (args[0].Contains("?"))
+                {
+                    Console.WriteLine("p    Pause nach der Ausführung");
+                    Environment.Exit(0);
+                }
+                else if (args[0].Contains("p"))
+                    pause = true;
+            }
+
+            Helper.Ausgabe(ConsoleColor.White, "Optionen laden");
+
+            var pfad = AppDomain.CurrentDomain.BaseDirectory;
+            var pfadDaten = pfad + "Daten";
+            var dateiDb = pfadDaten + @"\JgBackUp.bak";
+            var tagAkt = DateTime.Now.DayOfWeek.ToString().Substring(0, 2);
+
+            var tagAlt = "";
+            var dateiTag = pfadDaten + @"\MerkeTag.bin";
+            if (File.Exists(dateiTag))
+                tagAlt = File.ReadAllText(dateiTag);
+            if (tagAlt != tagAkt)
+            {
+                File.WriteAllText(dateiTag, tagAkt);
+                if (File.Exists(dateiDb))
+                    File.Delete(dateiDb);
+            }
+
+            var dateiOpt = pfad + "opt.txt";
+            if (!File.Exists(dateiOpt))
+            {
+                Helper.Ausgabe(ConsoleColor.Red, dateiOpt + " wurde nicht gefunden.");
+                Environment.Exit(0);
+            }
+            var optionen = await File.ReadAllLinesAsync(pfad + "opt.txt");
+            var opt = new Dictionary<string, string>();
+
+            foreach (var zeile in optionen)
+            {
+                var feld = zeile.Split(new char[] { ':' });
+                if (feld.Length > 1)
+                    opt.Add(feld[0].Trim().ToLower(), feld[1].Trim());
+            }
+
+            if (!Directory.Exists(pfadDaten))
+                Directory.CreateDirectory(pfadDaten);
+
+            Helper.Ausgabe(ConsoleColor.Yellow, "Beginne DB Update");
+
+            Helper.Backup(opt["database"], opt["sqlverbindung"], dateiDb);
+
+            Helper.Ausgabe(ConsoleColor.White, "Datei kopieren");
+
+            File.Copy(dateiDb, $"{pfadDaten}\\{opt["database"]}_{tagAkt}", true);
+
+            Helper.Ausgabe(ConsoleColor.White, "Upload auf FTP Server");
+
+            var timer = new Timer(5000);
+            timer.Elapsed += (sender, e) => Console.Write(".");
+            timer.Start();
+
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    client.Credentials = new NetworkCredential(opt["ftpusername"], opt["ftpkennwort"]);
+                    client.UploadFile($"ftp://{opt["ftphost"]}/{opt["database"]}.bak", WebRequestMethods.Ftp.UploadFile, dateiDb);
+                }
+            }
+            catch (Exception ex)
+            {
+                Helper.Ausgabe(ConsoleColor.Red, "Fehler beim Upload auf FTP Server. " + ex.Message);
+            }
+
+            timer.Stop();
+            Console.WriteLine("transfare abgeschlossen");
+
+            Helper.Ausgabe(ConsoleColor.White, "Sicherung erfolgreich abgeschlossen.");
+
+            if (pause)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Beenden mit Enter");
+                Console.ReadLine();
+            }
+        }
+    }
+}
